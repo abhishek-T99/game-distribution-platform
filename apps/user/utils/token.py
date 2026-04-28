540305118a2f433178c390aaf836bac1079495fc
Abhishek Timilsina <timilsina.abhishek55@gmail.com>
2026-04-28 09:18:39 +0545

    chore: code cleanup

--- apps/user/utils/token.py
+++ apps/user/utils/token.py
@@ -94,6 +94,5 @@ def just_for_commit():
     This is necessary because some version control systems may not track empty files, and we want to ensure that this utility file is included in the repository.
     Committtt
     Committtt
-    Committtt
     """
     return
