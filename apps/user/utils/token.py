c0a17e7084225767da23cb16eb7f1955d956c5eb
Abhishek Timilsina <timilsina.abhishek55@gmail.com>
2026-04-29 09:23:04 +0545

    chore: unwanted comments remove

--- apps/user/utils/token.py
+++ apps/user/utils/token.py
@@ -93,5 +93,6 @@ def just_for_commit():
     It can be removed or modified as needed.
     This is necessary because some version control systems may not track empty files, and we want to ensure that this utility file is included in the repository.
     Commit 1
+    Commit 2
     """
     return
